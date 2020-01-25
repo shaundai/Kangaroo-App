96358f42fe7da21e5a510293cd7e6d81e22a8200
Shaundai Person <shaundaiperson@gmail.com>
2020-01-24 22:38:46 -0500

    add functionality to take text input in addItemForm

--- screens/AddItemScreen.js
+++ screens/AddItemScreen.js
@@ -2,36 +2,27 @@ import React, { useState } from 'react';
 import { ScrollView, StyleSheet, Button, View } from 'react-native';
 import { TextInput } from 'react-native';
 
-export default function AddItemScreen({ nameRef, descRef, ownerRef, boxRef, locationRef, quantityRef}) {
+export default function AddItemScreen({ name, desc, owner, box, location, quantity}) {
   const [newItem, setNewItem] = useState({ name: '', desc: '', owner: '', box: '', location: '', quantity: ''});
 
+  name = 'name';
+  desc = 'desc';
+  owner = 'owner';
+  box = 'box';
+  location = 'location';
+  quantity = 'quantity';
 
-
-  handleChangeText = (text) => {
+  handleChangeText = (text, itemPart) => {
     const newItemPart = {...newItem};
-    newItemPart[`${itemPart}`] = text;//how to get itemPart to work dynamically?
+    newItemPart[`${itemPart}`] = text;
     setNewItem(newItemPart)
     console.log(newItemPart)
     };
 
-
-    
-  nameRef = React.createRef();
-  descRef = React.createRef();
-  ownerRef = React.createRef();
-  boxRef = React.createRef();
-  locationRef = React.createRef();
-  quantityRef = React.createRef();
-
   const createItem = (e) => {
       e && e.preventDefault();
       const item = {
-          name: nameRef.current.input._lastNativeText,
-          desc: descRef.current.input._lastNativeText,
-          owner: ownerRef.current.input._lastNativeText,
-          box: boxRef.current.input._lastNativeText,
-          location: locationRef.current.input._lastNativeText,
-          quantity: quantityRef.current.input._lastNativeText,
+         newItem ///add this item to redux state
       }
       //addItem(item);
       //e.currentTarget.reset(); this needs to be a new global state
@@ -41,12 +32,12 @@ export default function AddItemScreen({ nameRef, descRef, ownerRef, boxRef, loca
   return (
     <ScrollView style={styles.container}>
       <View>
-      <TextInput name="name" ref={nameRef} value={newItem.name} placeholder="What is this?" onChangeText={text => handleChangeText(text)} required></TextInput>
-      <TextInput name="desc" ref={descRef} value={newItem.desc} placeholder="Describe this item."></TextInput>
-      <TextInput name="owner" ref={ownerRef} value={newItem.owner} placeholder="Who does this belong to? (ex. the cat, Sally)"></TextInput>
-      <TextInput name="box" ref={boxRef} value={newItem.box} placeholder="Which box?" required></TextInput>
-      <TextInput name="location" ref={locationRef} value={newItem.location} placeholder="Where is this box?"></TextInput>
-      <TextInput name="quantity" ref={quantityRef} value={newItem.quantity} placeholder="Quantity"></TextInput>
+      <TextInput value={newItem.name} placeholder="What is this?" onChangeText={text => handleChangeText(text, name)} required></TextInput>
+      <TextInput value={newItem.desc} placeholder="Describe this item." onChangeText={text => handleChangeText(text, desc)} ></TextInput>
+      <TextInput value={newItem.owner} placeholder="Who does this belong to? (ex. the cat, Sally)" onChangeText={text => handleChangeText(text, owner)} ></TextInput>
+      <TextInput value={newItem.box} placeholder="Which box?" onChangeText={text => handleChangeText(text, box)} required></TextInput>
+      <TextInput value={newItem.location} placeholder="Where is this box?" onChangeText={text => handleChangeText(text, location)} ></TextInput>
+      <TextInput value={newItem.quantity} placeholder="Quantity" onChangeText={text => handleChangeText(text, quantity)} ></TextInput>
       <Button title="Add Item"
       onPress={()=> createItem()}/>
       </View>
