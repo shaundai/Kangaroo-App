dd2f6de324de739e40df93d3d3b91132694debf0
Shaundai Person <shaundaiperson@gmail.com>
2020-01-26 22:42:39 -0500

    add ability to delete item

--- screens/InventoryScreen.js
+++ screens/InventoryScreen.js
@@ -1,29 +1,40 @@
 import React from 'react';
 import { View, FlatList } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
+import { ListItem, Text, Button, ButtonGroup } from 'react-native-elements'
+import { deleteItem } from '../src/actions/index';
 import Item from '../src/components/Item';
-import { useSelector } from 'react-redux';
-import { ListItem } from 'react-native-elements'
+import { TouchableHighlight } from 'react-native-gesture-handler';
 
 //Components
 //TODO add additemform at the top
-//import EditItemForm from './EditItemForm'
+//import EditItemForm from './EditItemForm' 
 //import AddItemForm from './AddItemForm';
 
 export default function InventoryScreen(){
     const itemList = useSelector(state => state.itemListReducer);
     const list = itemList;
-
+    const dispatch = useDispatch();
     keyExtractor = (item, index) => index.toString()
+
+
 
     renderItem = ({ item }) => (
     <ListItem
         title={item.name}
         subtitle={item.desc}
+        subtitle={item.owner}
         leftAvatar={{ source: { uri: item.avatar_url } }}
-        rightElement={item.box}
         bottomDivider
         chevron
-    />
+        rightElement={<TouchableHighlight
+            onPress={()=>dispatch(deleteItem(item.id))}
+            activeOpacity={1}
+            underlayColor={"transparent"}
+            ><Text>Delete</Text></TouchableHighlight>}
+
+
+        />
 )
         return (
             <View>
