52906f928a238d7b4841947746a6e506f35a4235
Shaundai Person <shaundaiperson@gmail.com>
2020-02-09 22:59:02 -0500

    update swipe left to delete functionality

--- screens/InventoryScreen.js
+++ screens/InventoryScreen.js
@@ -6,7 +6,6 @@ import { deleteItem } from '../src/actions/index';
 import { SwipeListView } from 'react-native-swipe-list-view';
 import { Ionicons } from '@expo/vector-icons';
 
-
 //Components
 //TODO add additemform at the top
 //import EditItemForm from './EditItemForm' 
@@ -16,7 +15,7 @@ export default function InventoryScreen(){
     const itemList = useSelector(state => state.itemListReducer);
     const list = itemList;
     const dispatch = useDispatch();
-    keyExtractor = (item, index) => index.toString()
+    //const keyExtractor = item => 
 
     renderHiddenItem = ({item}) => (
         <View style={styles.hiddenDelete}>
@@ -27,29 +26,31 @@ export default function InventoryScreen(){
             </TouchableHighlight>
             </View>
     )
-
+    
     renderItem = ({ item }) => (
     <ListItem
         title={item.name}
         subtitleNumberOfLines={2}
         subtitle={item.desc}
         leftAvatar={{ source: { uri: item.avatar_url } }}
+        badge={{value: item.quantity, badgeStyle: {backgroundColor: '#000'}}}
         bottomDivider
         chevron
         />
 )
+
         return (
             <View>
                 <SwipeListView
                     useFlatList={true}
                     data={list}
                     renderItem={renderItem}
-                    keyExtractor={keyExtractor}
                     renderHiddenItem={renderHiddenItem}
+                    keyExtractor={item => item.id}
                     leftOpenValue={0}
                     rightOpenValue={-75}
-                    onRowOpen={(rowKey, rowMap, {item}) => {
-                    setTimeout(() => {item &&
+                    onRowOpen={(rowKey, rowMap) => {
+                    setTimeout(() => {
                         rowMap[rowKey].closeRow()
                     }, 2000)
     }}
